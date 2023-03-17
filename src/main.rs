841d1096b4f29fcd4ddb1c17b452247ed4dcf496
Jared Chesebro <jaredchesebro@novuslabs.com>
2023-03-17 13:03:51 -0700

    display either letter or underscore logic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-extern crate rand;
+// extern crate rand;
 use rand::Rng;
 
 use std::fs::File;
 use std::io::prelude::*;
 
+struct Letter {
+    character: char,
+    revealed: bool
+}
+
 fn main() {
     let selected_word = select_word();
+    // split the selected word into individual letters
+    let letters = create_letters(&selected_word);
 
-    println!("The selected word was {}", selected_word);
+    display_progress(&letters);
 }
 
 fn select_word() -> String {
@@ -27,4 +34,38 @@ fn select_word() -> String {
     let random_index = rand::thread_rng().gen_range(0..available_words.len());
 
     return String::from(available_words[random_index]);
+}
+
+
+fn create_letters(word: &String) -> Vec<Letter> {
+    // Create an empty vector to store letters
+    let mut letters: Vec<Letter> = Vec::new();
+
+// Wrap each character in a Letter struct
+    for c in word.chars() {
+        letters.push(Letter {
+            character: c,
+            revealed: false
+        });
+    }
+
+    return letters;
+}
+// Display appropriate character (Either a letter or an _)
+fn display_progress(letters: &Vec<Letter>) {
+    let mut display_string = String::from("Progress:"); // _ _ A _ Y _ S
+
+    for letter in letters {
+        display_string.push(' ');
+
+        if letter.revealed{
+            display_string.push(letter.character);
+        } else {
+            display_string.push('_');
+        }
+
+        display_string.push(' ');
+    }
+
+    println!("{}", display_string);
 }
